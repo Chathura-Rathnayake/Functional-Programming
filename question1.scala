b5c23356442b131d457e9d1250fdd998bbeda4b3
Chathura Rathnayake <c.rathnayake97@gmail.com>
2020-05-22 16:30:59 +0530

    Completed the question 1

--- question1.scala
+++ question1.scala
@@ -1,3 +1,11 @@
-object question1 extends App {
-  
+object question1 extends App
+{
+  def CelciusToFahrenheit( x:Double) : Double =
+  {
+    var answer:Double = 0
+    answer = (x * (1.8)) + 32
+    return answer
+  }
+  println(CelciusToFahrenheit(35.0)+ " °F")
+
 }
