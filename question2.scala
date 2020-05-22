7f4b6246d73f3f159fb54fd82769833f6308e20b
Chathura Rathnayake <c.rathnayake97@gmail.com>
2020-05-22 16:47:07 +0530

    Completed the second question

--- question2.scala
+++ question2.scala
@@ -1,5 +1,11 @@
 object question2 extends App
 {
-  
+  def SphereVolume( r:Double) : Double =
+  {
+    var volume:Double = (4.toDouble/3)*(Math.PI)*(Math.pow(r,3))
+    return volume
+  }
+  println(SphereVolume(5))
+
 
 }
