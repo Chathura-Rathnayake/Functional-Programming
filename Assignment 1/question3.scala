0ddb0f0d1f7de5afaa225b45e359b2a9dca06da9
Chathura97 <49788133+Chathura97@users.noreply.github.com>
2020-05-23 18:13:53 +0530

    Add an assumption for shipping costs

--- Assignment 1/question3.scala
+++ Assignment 1/question3.scala
@@ -2,6 +2,11 @@ object question3 extends App
 {
   def TotalCost( copies:Int) : Double =
   {
+    /*
+        Assumptions 
+              Meaning of this part in the question -> "Shipping costs Rs. 3 for the first 50 copy" is, 
+              the total amount for all the 50 books are Rs. 3.00 and it's not Rs. 3.00 for each book.
+    */
     var bookCosts:Double = 24.95*0.6*copies //calculates the cost for books
 
     var shippingCosts:Double = 3
